daa032c0241d265176578db5f2d82b5d022d9648
Shreyesh <shreyeshpachpor06@gmail.com>
2026-06-08 17:48:58 +0530

    feat: set up server with dotenv configuration and middleware integration

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,10 @@
-import "dotenv/config";
+import dotenv from "dotenv";
 import app from "./src/app.js";
 import { connectDB } from "./src/config/db.js";
 
 app.use(dotenv.config());
+
+await connectDB();
 
 app.listen(3000, () => {
   console.log("Server is not running on the port 3000");
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,5 +1,11 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
 
 const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cookieParser());
 
 export default app;
